08e3b7e61934c679981810fb0faece2bfa90f7f7
Ad Soyad <mail@ornek.com>
2026-06-21 16:57:51 +0300

    Fix platform-aware sharing build

--- lib/presentation/screens/buy_credits_screen.dart
+++ lib/presentation/screens/buy_credits_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:soframda_ne_eksik/core/localization/app_locale_scope.dart';
